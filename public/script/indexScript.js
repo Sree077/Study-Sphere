ad0577117f1a71fd654aa79d5b9aaea9d27c28d2
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2024-03-02 05:45:38 +0530

    bg-change

--- public/script/indexScript.js
+++ public/script/indexScript.js
@@ -124,3 +124,18 @@ location.reload()
     timerDisplayMinute.value = 5;
     timerDisplaySecond.value = 0;
  }
+
+//Raindrop
+ function createRaindrop() {
+  const raindrop = document.createElement('div');
+  raindrop.classList.add('raindrop');
+  raindrop.style.left = `${Math.random() * 100}%`;
+  document.getElementById('rainy-background').appendChild(raindrop);
+
+  // Remove raindrop after it falls
+  setTimeout(() => {
+    raindrop.remove();
+  }, 5000); // Match this with the CSS animation duration
+}
+
+setInterval(createRaindrop, 22); // Adjust the frequency as needed
